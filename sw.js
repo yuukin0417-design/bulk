61782739d39806db75e46acedc928bda4e5a560e
yuukin0417-design <yuuki.n0417@gmail.com>
2026-08-08 16:06:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bulk-v1';
+const CACHE = 'bulk-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
